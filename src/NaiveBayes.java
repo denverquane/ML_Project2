b7fc4048b985ee470dd8c5c20b31909b9bc1573f
Denver Quane <dquane@unm.edu>
2017-10-17 01:17:29 -0600

    Added code for cross-validation, Confusion Matrix output, and varying values of Beta

--- src/NaiveBayes.java
+++ src/NaiveBayes.java
@@ -6,14 +6,19 @@ import java.util.Scanner;
 
 public class NaiveBayes
 {
-  public static final int COLS = 61190;
-  public static final int UNIQUE_WORDS = COLS-2;
-  public static final int ROWS = 12000;
-  public static final double BETA = (double)(1.0 / UNIQUE_WORDS);
+  private static final int COLS = 61190;
+  private static final int UNIQUE_WORDS = COLS-2;
+  private static final int ROWS = 12000;
+  private static final boolean PRINT_MISCLASSIFY = false;
+  private static double BETA = 0.0000001;
+  //private static double BETA = 1.0 / (double)(UNIQUE_WORDS);
+
+  private static final boolean CROSS_VALIDATE = true;
 
   public static void main(String[] args){
     NaiveBayes csv = new NaiveBayes();
-    csv.trainAndClassify();
+    if(CROSS_VALIDATE) for(BETA = 0.00001; BETA < 1.1; BETA *= 10.0) csv.trainAndClassify();
+    else csv.trainAndClassify();
   }
 
   private void trainAndClassify(){
@@ -84,7 +89,7 @@ public class NaiveBayes
     for(int newsGroup = 0; newsGroup < 20; newsGroup++){
       int uniqueWords = uniqueWordsPerClass[newsGroup];
       for(int word = 0; word < UNIQUE_WORDS; word++){
-        double likelihood = (double)(wordOccurrencesInClass[newsGroup][word] + BETA) / (double)(uniqueWords + (UNIQUE_WORDS * BETA));
+        double likelihood = ((double)wordOccurrencesInClass[newsGroup][word] + BETA) / ((double)uniqueWords + ((double)UNIQUE_WORDS * BETA));
         array[newsGroup][word] = Math.log(likelihood);
       }
     }
@@ -122,14 +127,30 @@ public class NaiveBayes
 
   private void classifyTestData(double[] logPriors, double[][] likelihoods) throws FileNotFoundException, UnsupportedEncodingException
   {
-    Scanner sc2 = new Scanner(new File("data/testing.csv"));
-    PrintWriter writer = new PrintWriter("out.csv", "UTF-8");
-    writer.println("id,class");
+    Scanner sc2;
+    PrintWriter writer;
+    int[][] confusionMatrix;
+    int totalEvaluated = 0;
+    int totalCorrect = 0;
+
+    if(CROSS_VALIDATE) {
+      sc2 = new Scanner(new File("data/training.csv"));
+      confusionMatrix = new int[20][20];
+    }
+    else
+    {
+      sc2 = new Scanner(new File("data/testing.csv"));
+      writer = new PrintWriter("out.csv", "UTF-8");
+      writer.println("id,class");
+    }
+
 
     while(sc2.hasNext()){
       String s2 = sc2.next();
+      totalEvaluated++;
       String ls2[] = s2.split(",");
       int[] input = new int[UNIQUE_WORDS];
+      int actualClass;
 
       for(int i = 1; i < COLS-1; i++){
         int elem =  Integer.parseInt(ls2[i]);
@@ -141,9 +162,37 @@ public class NaiveBayes
 
       int bestClass = getBestClass(classProbs);
       //System.out.println("ID: " + ls2[0] + "Class: " + clas);
-      writer.println(ls2[0] + "," + bestClass);
+      if(!CROSS_VALIDATE)  writer.println(ls2[0] + "," + bestClass);
+      else{
+        actualClass = Integer.parseInt(ls2[COLS-1]);
+        if(actualClass != bestClass){
+          if(PRINT_MISCLASSIFY) System.out.println("ID: " + ls2[0] + " mismatch -> Predicted " + bestClass + " vs Actual " + actualClass);
+          confusionMatrix[bestClass - 1][actualClass - 1]++;
+        } else totalCorrect++;
+      }
     }
-    writer.close();
+    if(!CROSS_VALIDATE) {
+      writer.close();
+      System.out.println("Completed with Beta = " + BETA);
+    }
+    else{
+      if(PRINT_MISCLASSIFY)
+      {
+        for (int i = 0; i < 20; i++)
+        {
+          if (i < 9) System.out.print("Predicted " + (i + 1) + " : ");
+          else System.out.print("Predicted " + (i + 1) + ": ");
+          for (int j = 0; j < 20; j++)
+          {
+            int val = confusionMatrix[i][j];
+            if (val < 10) System.out.print(val + "  ");
+            else System.out.print(val + " ");
+          }
+          System.out.println();
+        }
+      }
+      System.out.println("\nPredicted " + totalCorrect + "/" + totalEvaluated + " Correctly (" + 100.0*((double)totalCorrect/(double)totalEvaluated) + "%) with Beta = " + BETA);
+    }
   }
 
   /**
