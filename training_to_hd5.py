24f2c32fc4bd85a708f78f2e5b4a5e4790882a42
Matthew <mmcchesney@unm.edu>
2017-10-18 01:02:44 -0600

    Python version almost working

--- training_to_hd5.py
+++ training_to_hd5.py
@@ -1,20 +1,63 @@
 from scipy import sparse
 import numpy as np
-import tables as tb 
 import pandas as pd
 import csv 
+import sys
+import math
+import operator
 
-cols = 61190
-rows = 12000
+def classify_data(likeli,priori):
+    with open('testing.csv') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            vals = np.zeros(20)
+            word = map(float,row)
+            firstElem = word.pop(0)
+            for k in range(int(19)):
+                like = map(float,likeli[k,:])
+                vals[k] = priori[k] * sum([i*j for i, j in zip(like,word)])
+            index, value = max(enumerate(vals), key=operator.itemgetter(1))
+            print (str(firstElem) + ', ' + str(index+1));
 
-# https://stackoverflow.com/a/27203245
+def calc_likeli(uniqWord, classArr, beta):
+    likeliArray = np.zeros(shape=(20,61188));
+    for i in range(len(classArr)):
+        for j in range(len(classArr[0])):
+            likeliArray[i][j] = (classArr[i][j] + beta)/(uniqWord[i] + 61188 * beta);
+    return likeliArray;
 
-filename = "training.h5"
+def calc_priori(data, occurArray):
+    for i in range(len(occurArray)):
+        occurArray[i] /= 12000.00;
+    return occurArray;
 
-df = pd.DataFrame.from_csv("training.csv", header=None, index_col=None)
-# makes a dataframe from the csv data (should make it easier for some ops.)
+def uniqueWords(newsGroups):
+    uniqArr = np.zeros(20);
+    for i in range(len(newsGroups)):
+        for j in range(len(newsGroups[0])):
+            if(newsGroups[i][j] > 0):
+                uniqArr[i] += 1;
+    return uniqArr;
 
-df.to_hdf(filename, 'data', mode = 'w')
-# converts the entire dataset to an hd5 file 
-
-print(pd.read_hdf(filename, 'data'))
+if __name__== "__main__":
+    clas = 0;
+    occurArray = np.zeros(20);
+    likeliArr = np.zeros(shape=(20,61188));
+    classArray = np.zeros(shape=(20,61188));
+    uniqArr = np.zeros(20);
+    #data = pd.read_csv("training.csv");
+    #data = np.array(list(csv.reader(open("training.csv", "rb"), delimiter=","))).astype("int");
+    reader = csv.reader(open('training.csv', 'r'), delimiter=",")
+    data = list(reader)
+    print("Done reading file");
+    for i in range (len(data)):
+        clas = int(data[i][len(data[0])-1]);                
+        occurArray[clas-1]+=1;
+        for j in range (1,len(data[0])-2):
+            if(data[i][j] != '0'): 
+                classArray[clas-1][j] += 1;
+    occurArray = calc_priori(data,occurArray);
+    del(data);
+    uniqArr = uniqueWords(classArray);
+    likeliArr = calc_likeli(uniqArr,classArray,1);
+    classify_data(likeliArr,occurArray);
